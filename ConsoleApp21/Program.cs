a6213c655230ea3e9dcfa5f012fbea8a3560ad5f
MetinAgayev <matinga@code.edu.az>
2021-12-11 23:47:57 +0400

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 6, 7, 8, 9, 93, 123, 432, 123 };
+
+            for (int i = arr.Length-1;0<i  ; i--)
+            {
+                Console.WriteLine(arr[i]);
+            } 
+           
+
+            
+           
+        }
+    }
+}
